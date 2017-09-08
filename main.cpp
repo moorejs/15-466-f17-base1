610c9800117277d5aa20f2ebca5af30d3bbc5ed8
Jared Moore <jmoore1@andrew.cmu.edu>
2017-09-07 21:45:42 -0400

    Formatted main, removed Makefiles

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 #include <iostream>
 #include <stdexcept>
 
-static GLuint compile_shader(GLenum type, std::string const &source);
+static GLuint compile_shader(GLenum type, std::string const& source);
 static GLuint link_program(GLuint vertex_shader, GLuint fragment_shader);
 
-int main(int argc, char **argv) {
-	//Configuration:
+int main(int argc, char** argv) {
+	// Configuration:
 	struct {
 		std::string title = "Game1: Text/Tiles";
 		glm::uvec2 size = glm::uvec2(640, 480);
@@ -21,10 +21,10 @@ int main(int argc, char **argv) {
 
 	//------------  initialization ------------
 
-	//Initialize SDL library:
+	// Initialize SDL library:
 	SDL_Init(SDL_INIT_VIDEO);
 
-	//Ask for an OpenGL context version 3.3, core profile, enable debug:
+	// Ask for an OpenGL context version 3.3, core profile, enable debug:
 	SDL_GL_ResetAttributes();
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
@@ -38,20 +38,18 @@ int main(int argc, char **argv) {
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
-	//create window:
-	SDL_Window *window = SDL_CreateWindow(
-		config.title.c_str(),
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		config.size.x, config.size.y,
-		SDL_WINDOW_OPENGL /*| SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI*/
-	);
+	// create window:
+	SDL_Window* window =
+			SDL_CreateWindow(config.title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, config.size.x,
+											 config.size.y, SDL_WINDOW_OPENGL /*| SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI*/
+											 );
 
 	if (!window) {
 		std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	//Create OpenGL context:
+	// Create OpenGL context:
 	SDL_GLContext context = SDL_GL_CreateContext(window);
 
 	if (!context) {
@@ -60,15 +58,15 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	#ifdef _WIN32
-	//On windows, load OpenGL extensions:
+#ifdef _WIN32
+	// On windows, load OpenGL extensions:
 	if (!init_gl_shims()) {
 		std::cerr << "ERROR: failed to initialize shims." << std::endl;
 		return 1;
 	}
-	#endif
+#endif
 
-	//Set VSYNC + Late Swap (prevents crazy FPS):
+	// Set VSYNC + Late Swap (prevents crazy FPS):
 	if (SDL_GL_SetSwapInterval(-1) != 0) {
 		std::cerr << "NOTE: couldn't set vsync + late swap tearing (" << SDL_GetError() << ")." << std::endl;
 		if (SDL_GL_SetSwapInterval(1) != 0) {
@@ -76,109 +74,113 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	//Hide mouse cursor (note: showing can be useful for debugging):
+	// Hide mouse cursor (note: showing can be useful for debugging):
 	SDL_ShowCursor(SDL_DISABLE);
 
 	//------------ opengl objects / game assets ------------
 
-	//texture:
+	// texture:
 	GLuint tex = 0;
-	glm::uvec2 tex_size = glm::uvec2(0,0);
+	glm::uvec2 tex_size = glm::uvec2(0, 0);
 
-	{ //load texture 'tex':
-		std::vector< uint32_t > data;
+	{	// load texture 'tex':
+		std::vector<uint32_t> data;
 		if (!load_png("elements.png", &tex_size.x, &tex_size.y, &data, LowerLeftOrigin)) {
 			std::cerr << "Failed to load texture." << std::endl;
 			exit(1);
 		}
-		//create a texture object:
+		// create a texture object:
 		glGenTextures(1, &tex);
-		//bind texture object to GL_TEXTURE_2D:
+		// bind texture object to GL_TEXTURE_2D:
 		glBindTexture(GL_TEXTURE_2D, tex);
-		//upload texture data from data:
+		// upload texture data from data:
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_size.x, tex_size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, &data[0]);
-		//set texture sampling parameters:
+		// set texture sampling parameters:
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	}
 
-	//shader program:
+	// shader program:
 	GLuint program = 0;
 	GLuint program_Position = 0;
 	GLuint program_TexCoord = 0;
 	GLuint program_Color = 0;
 	GLuint program_mvp = 0;
 	GLuint program_tex = 0;
-	{ //compile shader program:
+	{	// compile shader program:
 		GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER,
-			"#version 330\n"
-			"uniform mat4 mvp;\n"
-			"in vec4 Position;\n"
-			"in vec2 TexCoord;\n"
-			"in vec4 Color;\n"
-			"out vec2 texCoord;\n"
-			"out vec4 color;\n"
-			"void main() {\n"
-			"	gl_Position = mvp * Position;\n"
-			"	color = Color;\n"
-			"	texCoord = TexCoord;\n"
-			"}\n"
-		);
+																					"#version 330\n"
+																					"uniform mat4 mvp;\n"
+																					"in vec4 Position;\n"
+																					"in vec2 TexCoord;\n"
+																					"in vec4 Color;\n"
+																					"out vec2 texCoord;\n"
+																					"out vec4 color;\n"
+																					"void main() {\n"
+																					"	gl_Position = mvp * Position;\n"
+																					"	color = Color;\n"
+																					"	texCoord = TexCoord;\n"
+																					"}\n");
 
 		GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER,
-			"#version 330\n"
-			"uniform sampler2D tex;\n"
-			"in vec4 color;\n"
-			"in vec2 texCoord;\n"
-			"out vec4 fragColor;\n"
-			"void main() {\n"
-			"	fragColor = texture(tex, texCoord) * color;\n"
-			"}\n"
-		);
+																						"#version 330\n"
+																						"uniform sampler2D tex;\n"
+																						"in vec4 color;\n"
+																						"in vec2 texCoord;\n"
+																						"out vec4 fragColor;\n"
+																						"void main() {\n"
+																						"	fragColor = texture(tex, texCoord) * color;\n"
+																						"}\n");
 
 		program = link_program(fragment_shader, vertex_shader);
 
-		//look up attribute locations:
+		// look up attribute locations:
 		program_Position = glGetAttribLocation(program, "Position");
-		if (program_Position == -1U) throw std::runtime_error("no attribute named Position");
+		if (program_Position == -1U)
+			throw std::runtime_error("no attribute named Position");
 		program_TexCoord = glGetAttribLocation(program, "TexCoord");
-		if (program_TexCoord == -1U) throw std::runtime_error("no attribute named TexCoord");
+		if (program_TexCoord == -1U)
+			throw std::runtime_error("no attribute named TexCoord");
 		program_Color = glGetAttribLocation(program, "Color");
-		if (program_Color == -1U) throw std::runtime_error("no attribute named Color");
+		if (program_Color == -1U)
+			throw std::runtime_error("no attribute named Color");
 
-		//look up uniform locations:
+		// look up uniform locations:
 		program_mvp = glGetUniformLocation(program, "mvp");
-		if (program_mvp == -1U) throw std::runtime_error("no uniform named mvp");
+		if (program_mvp == -1U)
+			throw std::runtime_error("no uniform named mvp");
 		program_tex = glGetUniformLocation(program, "tex");
-		if (program_tex == -1U) throw std::runtime_error("no uniform named tex");
+		if (program_tex == -1U)
+			throw std::runtime_error("no uniform named tex");
 	}
 
-	//vertex buffer:
+	// vertex buffer:
 	GLuint buffer = 0;
-	{ //create vertex buffer
+	{	// create vertex buffer
 		glGenBuffers(1, &buffer);
 		glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	}
 
 	struct Vertex {
-		Vertex(glm::vec2 const &Position_, glm::vec2 const &TexCoord_, glm::u8vec4 const &Color_) :
-			Position(Position_), TexCoord(TexCoord_), Color(Color_) { }
+		Vertex(glm::vec2 const& Position_, glm::vec2 const& TexCoord_, glm::u8vec4 const& Color_)
+				: Position(Position_), TexCoord(TexCoord_), Color(Color_) {}
 		glm::vec2 Position;
 		glm::vec2 TexCoord;
 		glm::u8vec4 Color;
 	};
 	static_assert(sizeof(Vertex) == 20, "Vertex is nicely packed.");
 
-	//vertex array object:
+	// vertex array object:
 	GLuint vao = 0;
-	{ //create vao and set up binding:
+	{	// create vao and set up binding:
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
-		glVertexAttribPointer(program_Position, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLbyte *)0);
-		glVertexAttribPointer(program_TexCoord, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLbyte *)0 + sizeof(glm::vec2));
-		glVertexAttribPointer(program_Color, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (GLbyte *)0 + sizeof(glm::vec2) + sizeof(glm::vec2));
+		glVertexAttribPointer(program_Position, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLbyte*)0);
+		glVertexAttribPointer(program_TexCoord, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLbyte*)0 + sizeof(glm::vec2));
+		glVertexAttribPointer(program_Color, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex),
+													(GLbyte*)0 + sizeof(glm::vec2) + sizeof(glm::vec2));
 		glEnableVertexAttribArray(program_Position);
 		glEnableVertexAttribArray(program_TexCoord);
 		glEnableVertexAttribArray(program_Color);
@@ -191,23 +193,21 @@ int main(int argc, char **argv) {
 		glm::vec2 rad = glm::vec2(0.5f);
 	};
 
-
-	auto load_sprite = [](std::string const &name) -> SpriteInfo {
+	auto load_sprite = [](std::string const& name) -> SpriteInfo {
 		SpriteInfo info;
-		//TODO: look up sprite name in table of sprite infos
+		// TODO: look up sprite name in table of sprite infos
 		return info;
 	};
 
-
 	//------------ game state ------------
 
-	glm::vec2 mouse = glm::vec2(0.0f, 0.0f); //mouse position in [-1,1]x[-1,1] coordinates
+	glm::vec2 mouse = glm::vec2(0.0f, 0.0f);	// mouse position in [-1,1]x[-1,1] coordinates
 
 	struct {
 		glm::vec2 at = glm::vec2(0.0f, 0.0f);
 		glm::vec2 radius = glm::vec2(10.0f, 10.0f);
 	} camera;
-	//correct radius for aspect ratio:
+	// correct radius for aspect ratio:
 	camera.radius.x = camera.radius.y * (float(config.size.x) / float(config.size.y));
 
 	//------------ game loop ------------
@@ -216,10 +216,10 @@ int main(int argc, char **argv) {
 	while (true) {
 		static SDL_Event evt;
 		while (SDL_PollEvent(&evt) == 1) {
-			//handle input:
+			// handle input:
 			if (evt.type == SDL_MOUSEMOTION) {
 				mouse.x = (evt.motion.x + 0.5f) / float(config.size.x) * 2.0f - 1.0f;
-				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) *-2.0f + 1.0f;
+				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) * -2.0f + 1.0f;
 			} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
 			} else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
 				should_quit = true;
@@ -228,38 +228,38 @@ int main(int argc, char **argv) {
 				break;
 			}
 		}
-		if (should_quit) break;
+		if (should_quit)
+			break;
 
 		auto current_time = std::chrono::high_resolution_clock::now();
 		static auto previous_time = current_time;
-		float elapsed = std::chrono::duration< float >(current_time - previous_time).count();
+		float elapsed = std::chrono::duration<float>(current_time - previous_time).count();
 		previous_time = current_time;
 
-		{ //update game state:
+		{	// update game state:
 			(void)elapsed;
 		}
 
-		//draw output:
+		// draw output:
 		glClearColor(0.5, 0.5, 0.5, 0.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+		{	// draw game state:
+			std::vector<Vertex> verts;
 
-		{ //draw game state:
-			std::vector< Vertex > verts;
-
-			//helper: add rectangle to verts:
-			auto rect = [&verts](glm::vec2 const &at, glm::vec2 const &rad, glm::u8vec4 const &tint) {
-				verts.emplace_back(at + glm::vec2(-rad.x,-rad.y), glm::vec2(0.0f, 0.0f), tint);
+			// helper: add rectangle to verts:
+			auto rect = [&verts](glm::vec2 const& at, glm::vec2 const& rad, glm::u8vec4 const& tint) {
+				verts.emplace_back(at + glm::vec2(-rad.x, -rad.y), glm::vec2(0.0f, 0.0f), tint);
 				verts.emplace_back(verts.back());
 				verts.emplace_back(at + glm::vec2(-rad.x, rad.y), glm::vec2(0.0f, 1.0f), tint);
-				verts.emplace_back(at + glm::vec2( rad.x,-rad.y), glm::vec2(1.0f, 0.0f), tint);
-				verts.emplace_back(at + glm::vec2( rad.x, rad.y), glm::vec2(1.0f, 1.0f), tint);
+				verts.emplace_back(at + glm::vec2(rad.x, -rad.y), glm::vec2(1.0f, 0.0f), tint);
+				verts.emplace_back(at + glm::vec2(rad.x, rad.y), glm::vec2(1.0f, 1.0f), tint);
 				verts.emplace_back(verts.back());
 			};
 
-			auto draw_sprite = [&verts](SpriteInfo const &sprite, glm::vec2 const &at, float angle = 0.0f) {
+			auto draw_sprite = [&verts](SpriteInfo const& sprite, glm::vec2 const& at, float angle = 0.0f) {
 				glm::vec2 min_uv = sprite.min_uv;
 				glm::vec2 max_uv = sprite.max_uv;
 				glm::vec2 rad = sprite.rad;
@@ -270,19 +270,17 @@ int main(int argc, char **argv) {
 				verts.emplace_back(at + right * -rad.x + up * -rad.y, glm::vec2(min_uv.x, min_uv.y), tint);
 				verts.emplace_back(verts.back());
 				verts.emplace_back(at + right * -rad.x + up * rad.y, glm::vec2(min_uv.x, max_uv.y), tint);
-				verts.emplace_back(at + right *  rad.x + up * -rad.y, glm::vec2(max_uv.x, min_uv.y), tint);
-				verts.emplace_back(at + right *  rad.x + up *  rad.y, glm::vec2(max_uv.x, max_uv.y), tint);
+				verts.emplace_back(at + right * rad.x + up * -rad.y, glm::vec2(max_uv.x, min_uv.y), tint);
+				verts.emplace_back(at + right * rad.x + up * rad.y, glm::vec2(max_uv.x, max_uv.y), tint);
 				verts.emplace_back(verts.back());
 			};
 
-
-			//Draw a sprite "player" at position (5.0, 2.0):
-			static SpriteInfo player = load_sprite("player"); //TODO: hoist
+			// Draw a sprite "player" at position (5.0, 2.0):
+			static SpriteInfo player = load_sprite("player");	// TODO: hoist
 			draw_sprite(player, glm::vec2(5.0, 2.0), 0.2f);
 
 			rect(glm::vec2(0.0f, 0.0f), glm::vec2(4.0f), glm::u8vec4(0xff, 0x00, 0x00, 0xff));
 			rect(mouse * camera.radius + camera.at, glm::vec2(4.0f), glm::u8vec4(0xff, 0xff, 0xff, 0x88));
-
 
 			glBindBuffer(GL_ARRAY_BUFFER, buffer);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * verts.size(), &verts[0], GL_STREAM_DRAW);
@@ -291,12 +289,8 @@ int main(int argc, char **argv) {
 			glUniform1i(program_tex, 0);
 			glm::vec2 scale = 1.0f / camera.radius;
 			glm::vec2 offset = scale * -camera.at;
-			glm::mat4 mvp = glm::mat4(
-				glm::vec4(scale.x, 0.0f, 0.0f, 0.0f),
-				glm::vec4(0.0f, scale.y, 0.0f, 0.0f),
-				glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
-				glm::vec4(offset.x, offset.y, 0.0f, 1.0f)
-			);
+			glm::mat4 mvp = glm::mat4(glm::vec4(scale.x, 0.0f, 0.0f, 0.0f), glm::vec4(0.0f, scale.y, 0.0f, 0.0f),
+																glm::vec4(0.0f, 0.0f, 1.0f, 0.0f), glm::vec4(offset.x, offset.y, 0.0f, 1.0f));
 			glUniformMatrix4fv(program_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
 
 			glBindTexture(GL_TEXTURE_2D, tex);
@@ -305,10 +299,8 @@ int main(int argc, char **argv) {
 			glDrawArrays(GL_TRIANGLE_STRIP, 0, verts.size());
 		}
 
-
 		SDL_GL_SwapWindow(window);
 	}
-
 
 	//------------  teardown ------------
 
@@ -321,11 +313,9 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-
-
-static GLuint compile_shader(GLenum type, std::string const &source) {
+static GLuint compile_shader(GLenum type, std::string const& source) {
 	GLuint shader = glCreateShader(type);
-	GLchar const *str = source.c_str();
+	GLchar const* str = source.c_str();
 	GLint length = source.size();
 	glShaderSource(shader, 1, &str, &length);
 	glCompileShader(shader);
@@ -335,7 +325,7 @@ static GLuint compile_shader(GLenum type, std::string const &source) {
 		std::cerr << "Failed to compile shader." << std::endl;
 		GLint info_log_length = 0;
 		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_log_length);
-		std::vector< GLchar > info_log(info_log_length, 0);
+		std::vector<GLchar> info_log(info_log_length, 0);
 		GLsizei length = 0;
 		glGetShaderInfoLog(shader, info_log.size(), &length, &info_log[0]);
 		std::cerr << "Info log: " << std::string(info_log.begin(), info_log.begin() + length);
@@ -356,7 +346,7 @@ static GLuint link_program(GLuint fragment_shader, GLuint vertex_shader) {
 		std::cerr << "Failed to link shader program." << std::endl;
 		GLint info_log_length = 0;
 		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_log_length);
-		std::vector< GLchar > info_log(info_log_length, 0);
+		std::vector<GLchar> info_log(info_log_length, 0);
 		GLsizei length = 0;
 		glGetProgramInfoLog(program, info_log.size(), &length, &info_log[0]);
 		std::cerr << "Info log: " << std::string(info_log.begin(), info_log.begin() + length);
