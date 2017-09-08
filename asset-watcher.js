4a08cd3ae7588f8c79d576c58d8745b1bd09d1da
Jared Moore <jmoore1@andrew.cmu.edu>
2017-09-08 09:53:21 -0400

    Added auto jam and run executable

--- asset-watcher.js
+++ asset-watcher.js
@@ -1,45 +1,94 @@
+#!/usr/bin/env node
+
 "use strict";
 
 const fs = require("fs");
 const path = require("path");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const { exec } = require("child_process");
 
-const directory = path.resolve(process.cwd(), process.argv[2] || ".");
+const cwd = process.cwd();
+const directories = process.argv.slice(2).map(filepath => path.resolve(cwd, filepath));
 
-console.log(`Listening for changes to "${directory}"`);
+if (!directories.length) {
+  directories.push(path.resolve(cwd, "."));
+}
+
+console.log(`Listening for changes to:\n${directories.join("\n")}\n`);
+
+let proc;
 
 const onChange = {
-  ".xcf": ({ filename, name, fullpath }) => {
+  ".xcf": ({ filename, name, fullpath, directory }) => {
     console.log(`Transforming ${filename} to png...`);
 
-    exec(`convert -flatten ${fullpath} ${path.resolve(directory, name)}.png`)
-      .then(({ stdout, stderr }) => {
-        if (stderr) {
-          console.error(stderr);
+    exec(
+      `convert -flatten ${fullpath} ${path.resolve(directory, name)}.png`,
+      (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
         }
 
-        console.log(`Finished.\n`);
-      })
-      .catch(err => console.error(err));
+        if (stderr) {
+          console.error(stderr);
+          return;
+        }
+
+        if (stdout) {
+          console.log(stdout);
+        }
+
+        console.log(`Finished.`);
+
+        if (proc) {
+          onChange[".cpp"]();
+        }
+      }
+    );
+  },
+  ".cpp": () => {
+    console.log("Jamming file...");
+    exec("jam", (err, stdout, stderr) => {
+      if (proc) {
+        console.log("Killing previous executable...\n");
+        proc.kill();
+      }
+
+      // stderr vs err, not much different?
+      if (stderr) {
+        console.error(stderr);
+        return;
+      }
+
+      if (stdout) {
+        console.log(stdout);
+      }
+
+      console.log("Running executable...");
+      proc = exec("./dist/main");
+      proc.stdout.on("data", data => process.stdout.write(`main-stdout: ${data}`));
+      proc.stderr.on("data", err =>
+        process.stderr.write(`main-stderr: \x1b[4m\x1b[31m${err}\x1b[0m`)
+      );
+    });
   }
 };
 
 // TODO: allow options, such as verbose mode that prints commands
 
 console.log(
-  `Currently performing operations on the following filetypes: ${Object.keys(onChange).join(",")}`
+  `Currently performing operations on the following filetypes: ${Object.keys(onChange).join(", ")}`
 );
 
 console.log("Press Control + C to exit.\n");
 
-const watcher = fs.watch(directory, (eventType, filename) => {
+const watcherCallback = directory => (eventType, filename) => {
   if (!filename) {
     console.error("No filename provided...");
   }
 
   const file = {
     filename,
+    directory,
     extension: filename.slice(filename.lastIndexOf(".")), // including .
     name: filename.slice(0, filename.lastIndexOf(".")), // without extension
     fullpath: path.resolve(directory, filename)
@@ -48,6 +97,7 @@ const watcher = fs.watch(directory, (eventType, filename) => {
   switch (eventType) {
     case "change":
       if (typeof onChange[file.extension] === "function") {
+        console.log(`\n${filename} changed. Processing...\n`);
         onChange[file.extension](file);
       }
 
@@ -58,8 +108,16 @@ const watcher = fs.watch(directory, (eventType, filename) => {
 
       break;
   }
-});
+};
+
+const watchers = directories.map(directory => fs.watch(directory, watcherCallback(directory)));
 
 process.once("SIGINT", () => {
-  watcher.close();
+  console.log("\nClosing up shop...");
+
+  watchers.forEach(watcher => watcher.close());
+
+  if (proc) {
+    proc.kill();
+  }
 });
